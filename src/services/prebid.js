2daacb140f798076b253f129e0a172031c0bd461
Christian Gazmuri <christiangasmuri@Mac.attlocal.net>
2025-06-23 05:58:37 -0400

    [PREP-2777-Magnite-prebid-setup] - removed all magnite stuff to test if it will continue to work

--- src/services/prebid.js
+++ src/services/prebid.js
@@ -24,25 +24,19 @@ export function fetchPrebidBidsArray(ad, codes, timeout, info, prerender, cb = n
     return;
   }
 
-  const magniteAds = ['HP01', 'HP02', 'RP01', 'RP02', 'RP04', 'PG01', 'PG02', 'PG03', 'RPAA', 'VP01', 'VP02'];
-
-  if (magniteAds.includes(codes[0])) {
-    console.log('yo');
-  } else {
-    pbjs.requestBids({
-      timeout,
-      adUnitCodes: codes,
-      bidsBackHandler: (result) => {
-        console.log('Bid Back Handler', result);
-        pbjs.setTargetingForGPTAsync(codes);
-        if (cb) {
-          cb();
-        } else {
-          refreshSlot({ ad, info, prerender });
-        }
-      },
-    });
-  }
+  pbjs.requestBids({
+    timeout,
+    adUnitCodes: codes,
+    bidsBackHandler: (result) => {
+      console.log('Bid Back Handler', result);
+      pbjs.setTargetingForGPTAsync(codes);
+      if (cb) {
+        cb();
+      } else {
+        refreshSlot({ ad, info, prerender });
+      }
+    },
+  });
 }
 
 export function fetchPrebidBids(ad, code, timeout, info, prerender, cb = null) {
